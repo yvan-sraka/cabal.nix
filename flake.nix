e0c0567638a44b2183c41ddff63ccf776604124f
Rebecca Turner <rbt@sent.as>
2024-08-30 16:57:24 -0700

    cabal-install: 3.10 -> 3.12
    This fixes a build error in the Cabal repo.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
